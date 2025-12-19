d2bc5e38342808fd633ae2d0918960799120fb19
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-12-18 23:29:51 -0400

    Add Dashboard page with user fetching and logout functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { Routes, Route } from 'react-router';
 import AuthLayout from './layout/AuthLayout';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 export default function App() {
     return (
         <Routes>
+            <Route path='dashboard' element={<Dashboard />} />
+
             <Route element={<AuthLayout />}>
                 <Route index element={<Login />} />
                 <Route path="register" element={<Register />} />
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import AppLayout from '../layout/AppLayout';
+import axios, { isAxiosError } from 'axios';
+import { useLoading, useNotification } from '../context/store';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+
+export default function Dashboard() {
+    const { changeLoadingStatus } = useLoading();
+    const { showNotification } = useNotification();
+    const navigate = useNavigate();
+    const [user, setUser] = useState<any>();
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const fetchUser = async () => {
+            try {
+                changeLoadingStatus(true);
+                const getUserUrl = `${import.meta.env.VITE_BASE_URL}/user`;
+                const { data } = await axios.get(getUserUrl, { withCredentials: true });
+                setUser(data.data);
+            } catch (err) {
+                if (axios.isCancel(err)) return;
+                if (isAxiosError(err)) {
+                    if (err.response?.status === 401) {
+                        navigate('/');
+                    }
+                    console.error(err.message);
+                }
+            } finally {
+                changeLoadingStatus(false);
+            }
+        };
+
+        fetchUser();
+        return () => controller.abort();
+    }, [changeLoadingStatus, navigate]);
+
+    const handleLogout = async () => {
+        try {
+            const logoutUrl = `${import.meta.env.VITE_BASE_URL}/auth/logout`;
+            changeLoadingStatus(true);
+            await axios.post(logoutUrl, {}, { withCredentials: true });
+            showNotification('Log out successful');
+            setUser({});
+            navigate('/');
+        } catch (err) {
+            if (axios.isCancel(err)) return;
+            if (isAxiosError(err)) {
+                if (err.response?.status === 401) {
+                    navigate('/');
+                }
+                console.error(err.message);
+            }
+        } finally {
+            changeLoadingStatus(false);
+        }
+    };
+
+    if(user)return (
+        <AppLayout>
+            <button onClick={handleLogout}>Log out</button>
+        </AppLayout>
+    );
+}
