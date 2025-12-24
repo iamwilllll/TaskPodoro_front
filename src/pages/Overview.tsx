6876b6fb0b4706e51b920b20e5bd470a38f8f008
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-12-24 00:53:41 -0400

    Feat: Fetch groups in Overview page

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,3 +1,72 @@
+import axios, { isAxiosError } from 'axios';
+import { useLoading } from '../context';
+import { useEffect } from 'react';
+import GroupsItem from '../components/GroupsItem';
+
 export default function Overview() {
-    return <div className="col-start-2 col-end-10 row-start-2 row-end-10 size-full">Overview</div>;
+    const { changeLoadingStatus } = useLoading();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const url = `${import.meta.env.VITE_BASE_URL}/user/getAllGroups`;
+                changeLoadingStatus(true);
+                const response = await axios.get(url, { withCredentials: true });
+
+                console.log(response.data.groups);
+            } catch (err) {
+                if (isAxiosError(err)) {
+                    console.log(err.response?.data.message);
+                }
+
+                console.log(err);
+            } finally {
+                changeLoadingStatus(false);
+            }
+        })();
+    }, [changeLoadingStatus]);
+
+    return (
+        <div className="flex size-full flex-col gap-5 lg:flex-row lg:justify-between lg:gap-0">
+            <GroupsItem
+                name="Group"
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYhMQ5XV2jsvaxUqzdbtLUJ_cbaQYCdR4ORw&s"
+                description="group description test test test test test group description group description"
+                id="694b583274d448760590de7d"
+                className='h-75'
+            />
+            <GroupsItem
+                name="Group"
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYhMQ5XV2jsvaxUqzdbtLUJ_cbaQYCdR4ORw&s"
+                description="group description test test test test test group description group description"
+                id="694b583274d448760590de7d"
+                className='h-75'
+
+            />
+            <GroupsItem
+                name="Group"
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYhMQ5XV2jsvaxUqzdbtLUJ_cbaQYCdR4ORw&s"
+                description="group description test test test test test group description group description"
+                id="694b583274d448760590de7d"
+                className='h-75'
+
+            />
+            <GroupsItem
+                name="Group"
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYhMQ5XV2jsvaxUqzdbtLUJ_cbaQYCdR4ORw&s"
+                description="group description test test test test test group description group description"
+                id="694b583274d448760590de7d"
+                className='h-75'
+
+            />
+            <GroupsItem
+                name="Group"
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYhMQ5XV2jsvaxUqzdbtLUJ_cbaQYCdR4ORw&s"
+                description="group description test test test test test group description group description"
+                id="694b583274d448760590de7d"
+                className='h-75'
+
+            />
+        </div>
+    );
 }
